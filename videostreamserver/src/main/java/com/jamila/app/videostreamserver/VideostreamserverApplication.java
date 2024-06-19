99c31bfd8b6a60fe6debb077d165175b8896d7ea
Anand Gautam <jamiladeveloper@gmail.com>
2024-06-19 11:11:34 +0530

    Intial setup completed - server and ui projects created

---
+++ videostreamserver/src/main/java/com/jamila/app/videostreamserver/VideostreamserverApplication.java
@@ -0,0 +1,13 @@
+package com.jamila.app.videostreamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostreamserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostreamserverApplication.class, args);
+	}
+
+}
---
+++ videostreamserver/src/test/java/com/jamila/app/videostreamserver/VideostreamserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamila.app.videostreamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
